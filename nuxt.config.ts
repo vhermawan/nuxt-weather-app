d1120059e2026414bbb5c55acf4914c7215a5816
Vicky Hermawan <107031434+vhermawan@users.noreply.github.com>
2023-11-10 16:29:05 +0700

    init repository

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  webpack: {
+    loaders: {
+      vue: {
+        hotReload: true,
+      }
+    }
+  },
+  devtools: { enabled: true },
+  modules: ["nuxt-ark-ui"]
+})
